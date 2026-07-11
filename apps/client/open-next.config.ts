21f0efff04de34590e4e98e268a7809e03625611
Amit123103 <amitakhil001@gmail.com>
2026-07-11 14:49:35 +0530

    fix: remove kv cache requirement to fix deployment error

--- apps/client/open-next.config.ts
+++ apps/client/open-next.config.ts
@@ -1,6 +1,3 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
-import kvCache from "@opennextjs/cloudflare/overrides/incremental-cache/kv-incremental-cache";
 
-export default defineCloudflareConfig({
-  incrementalCache: kvCache,
-});
+export default defineCloudflareConfig({});
